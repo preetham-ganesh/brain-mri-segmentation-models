eeafe9a7544f087e2f3e0bac0b129558bdbd9368
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-08-02 17:42:11 -0400

    [#4] added extract_data_from_zip_file() to Dataset class.
    Extracts files from downloaded data zip file.

--- training/src/flair_abnormality_segmentation/dataset.py
+++ training/src/flair_abnormality_segmentation/dataset.py
@@ -1,3 +1,8 @@
+import os
+import zipfile
+
+from src.utils import check_directory_path_existence
+
 from typing import Dict, Any
 
 
@@ -22,3 +27,45 @@ class Dataset(object):
 
         # Initalizes class variables.
         self.model_configuration = model_configuration
+
+    def extract_data_from_zip_file(self) -> None:
+        """Extracts files from downloaded data zip file.
+
+        Extracts files from downloaded data zip file.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # Creates absolute directory path for downloaded data zip file.
+        self.home_directory_path = os.getcwd()
+        zip_file_path = "{}/data/raw_data/archive.zip".format(self.home_directory_path)
+
+        # Creates the directory path.
+        extracted_data_directory_path = check_directory_path_existence(
+            "data/extracted_data/lgg_mri_segmentation"
+        )
+
+        # If file does not exist, then extracts files from the directory.
+        if not os.path.exists(
+            "{}/kaggle_3m/data.csv".format(extracted_data_directory_path)
+        ):
+
+            # Extracts files from downloaded data zip file into a directory.
+            try:
+                with zipfile.ZipFile(zip_file_path, "r") as zip_file:
+                    zip_file.extractall(extracted_data_directory_path)
+            except FileNotFoundError as error:
+                raise FileNotFoundError(
+                    "{} does not exist. Download data from ".format(zip_file_path)
+                    + "'https://www.kaggle.com/datasets/mateuszbuda/lgg-mri-segmentation', and place it in "
+                    + "'data/raw_data' as 'archive.zip'."
+                )
+            print(
+                "Finished extracting files from 'archive.zip' to {}.".format(
+                    extracted_data_directory_path
+                )
+            )
+            print()
