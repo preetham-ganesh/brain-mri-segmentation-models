3c582552c6d7f97eba0c251c8f63f9da216dd2e9
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-08-03 16:26:20 -0400

    [#5] added load_model_configuration() to Train class.
    Loads the model configuration file for model version.

--- training/src/flair_abnormality_segmentation/train.py
+++ training/src/flair_abnormality_segmentation/train.py
@@ -1,3 +1,8 @@
+import os
+
+from src.utils import load_json_file
+
+
 class Train(object):
     """Trains the segmentation model based on the configuration."""
 
@@ -18,3 +23,24 @@ class Train(object):
         # Initalizes class variables.
         self.model_version = model_version
         self.best_validation_loss = None
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for model version.
+
+        Loads the model configuration file for model version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = (
+            "{}/configs/models/flair_abnormality_segmentation".format(
+                self.home_directory_path
+            )
+        )
+        self.model_configuration = load_json_file(
+            "v{}".format(self.model_version), model_configuration_directory_path
+        )
