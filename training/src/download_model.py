5ab7a45a6cd8ce67061bb3908c7338b4a964bb60
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-08-08 17:05:14 -0400

    [#15] added download_model_from_s3_bucket().
    Downloads files the serialized model from S3 bucket individually.

--- training/src/download_model.py
+++ training/src/download_model.py
@@ -1,7 +1,76 @@
+import os
 import argparse
+import boto3
+import sys
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_PATH)
+
+
+from src.utils import check_directory_path_existence
+
+
+def download_model_from_s3_bucket(
+    model_name: str,
+    model_version: str,
+    s3_bucket_name: str,
+    s3_artifact_directory_path: str,
+) -> None:
+    """Downloads files the serialized model from S3 bucket individually.
+
+    Downloads files the serialized model from S3 bucket individually.
+
+    Args:
+        model_name: A string for name of the model in the S3 bucket.
+        model_version: A string for the version of the model in the S3 bucket.
+        s3_bucket_name: A string for the name of the bucket where the serialized model is stored.
+        s3_artifact_directory_path: A string for the URI of the artifact directory path in S3 bucket.
+
+    Returns:
+        None.
+    """
+    # Creates the absolute path for the directory path given in argument if it does not already exist.
+    serialized_model_directory_path = check_directory_path_existence(
+        "models/{}/v{}/model".format(model_name, model_version)
+    )
+
+    # Initializes S3 client
+    s3 = boto3.client("s3")
+
+    # Lists objects in the specified S3 directory.
+    s3_objects = s3.list_objects_v2(
+        Bucket=s3_bucket_name, Prefix=s3_artifact_directory_path
+    )
+
+    # Iterates across objects in the S3 directory.
+    for obj in s3_objects.get("Contents", []):
+        object_key = obj["Key"]
+
+        # Skips if object is a directory.
+        if object_key.endswith("/"):
+            continue
+
+        # Creates a relative file path for object.
+        object_file_path = os.path.join(
+            serialized_model_directory_path,
+            os.path.relpath(object_key, s3_artifact_directory_path),
+        )
+
+        # Extracts directory name of object file path. Create the directory if it does not exist.
+        object_sub_directory_path = os.path.dirname(object_file_path)
+        _ = check_directory_path_existence(object_sub_directory_path)
+
+        # Download the file
+        s3.download_file(s3_bucket_name, object_key, object_file_path)
+        print("Finished downloading {} to {}.".format(object_key, object_file_path))
+    print()
 
 
 def main():
+    print()
+
     # Parses the arguments.
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -34,8 +103,8 @@ def main():
     )
     args = parser.parse_args()
 
-    #
-    download_from_s3_bucket(
+    # Downloads files the serialized model from S3 bucket individually.
+    download_model_from_s3_bucket(
         args.model_name,
         args.model_version,
         args.s3_bucket_name,
